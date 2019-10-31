0c80d79be3efbb56a71357533953f967650a7268
Temiloluwa Ojo <themmyloluwaa@gmail.com>
2019-10-31 11:48:09 +0100

    added all files to git

---
+++ includes/db.php
@@ -0,0 +1,13 @@
+<?php
+
+$db_host = 'localhost';
+$db_user = 'root';
+$db_password = '';
+$db_name = 'study2japa';
+
+
+$connection = mysqli_connect($db_host, $db_user, $db_password, $db_name);
+
+
+
+?>
